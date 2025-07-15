2503aaa29f66fa1236b06053418374e84fc38cd7
mehmonov <uzbekfreedom@gmail.com>
2025-07-15 22:52:53 +0500

    refactor: comment out database configuration variables in config.py

--- bot/data/config.py
+++ bot/data/config.py
@@ -9,8 +9,8 @@ BOT_TOKEN = env.str("BOT_TOKEN")  # Bot Token
 ADMINS = env.list("ADMINS")  # adminlar ro'yxati
 
 
-DB_USER = env.str("DB_USER")
-DB_PASS = env.str("DB_PASS")
-DB_NAME = env.str("DB_NAME")
-DB_HOST = env.str("DB_HOST")
-DB_PORT = env.str("DB_PORT")
+# DB_USER = env.str("DB_USER")
+# DB_PASS = env.str("DB_PASS")
+# DB_NAME = env.str("DB_NAME")
+# DB_HOST = env.str("DB_HOST")
+# DB_PORT = env.str("DB_PORT")
